09d60d77f19afdb2bc969d2563607e634bb89ec0
Tim Gutwald <tim.gutwald@gmail.com>
2013-05-06 15:28:50 -0600

    Initial Commut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a63ee0a32837ec69e3962e2a2d68619ac79df704879415e8a06131e3c70c13e98786727a52a566aefc733ab502f2fd5ea626b6dd8dadd6ad8d9c86753ca865b6'
